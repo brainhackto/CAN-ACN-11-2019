d701aaef93bb840ec34be292cdbc4d64cecb8b09
jeyacha3 <jjeyachandra@myseneca.ca>
2019-04-02 10:40:35 -0400

    added breaks between submissions

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,7 +7,7 @@ $(function() {
             "<p>" + val.project_desc + "</p>" + "<p><b>Name: </b>" + val.user_name + "</p>" +
             "<p><b>Contact: </b>" + val.contact_address + "</p><p><b>GitHub: </b>" + val.github_link + "</p>" +
             "<p><b>Institution/Company: </b>" + val.affiliation + "</p><p><b>Areas of Interest: </b>" + val.area_of_interest + "</p>" +
-            "<p><b>Desired Skills: </b>" + val.desired_skills + "</p><p><b>Goals: </b>" + val.goals + "</p><br>");
+            "<p><b>Desired Skills: </b>" + val.desired_skills + "</p><p><b>Goals: </b>" + val.goals + "</p><br><hr>");
         });
         $("<div/>", {
             "class": "prj",
