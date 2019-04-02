d2178862b06a842672fb6480fddd42c854a29d66
jeyacha3 <jjeyachandra@myseneca.ca>
2019-04-02 10:22:42 -0400

    fixed js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,12 +2,12 @@ $(function() {
     
     $.getJSON("assets/data/submissions.json", function(data){
         let submissions = [];
-        $.each(data, function(){
-            submissions.push("<h3>" + data.project_title + "</h3>" +
-            "<p>" + data.project_desc + "</p>" + "<p><b>Name: </b>" + data.user_name + "</p>" +
-            "<p><b>Contact: </b>" + data.contact_address + "</p><p><b>GitHub: </b>" + data.github_link + "</p>" +
-            "<p><b>Institution/Company: </b>" + data.affiliation + "</p><p><b>Areas of Interest: </b>" + data.area_of_interest + "</p>" +
-            "<p><b>Desired Skills: </b>" + data.desired_skills + "</p><p><b>Goals: </b>" + data.goals + "</p><br>");
+        $.each(JSON.parse(data), function(i,val){
+            submissions.push("<h3>" + val.project_title + "</h3>" +
+            "<p>" + val.project_desc + "</p>" + "<p><b>Name: </b>" + val.user_name + "</p>" +
+            "<p><b>Contact: </b>" + val.contact_address + "</p><p><b>GitHub: </b>" + val.github_link + "</p>" +
+            "<p><b>Institution/Company: </b>" + val.affiliation + "</p><p><b>Areas of Interest: </b>" + val.area_of_interest + "</p>" +
+            "<p><b>Desired Skills: </b>" + val.desired_skills + "</p><p><b>Goals: </b>" + val.goals + "</p><br>");
         });
         $("<div/>", {
             "class": "prj",
